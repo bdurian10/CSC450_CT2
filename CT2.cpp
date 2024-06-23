cf77dabf77dd4d282e1c62c2b52f0b6db069bb72
Braden Durian <braden.durian@csuglobal.edu>
2024-06-23 15:01:52 -0600

    First Commit: created main program that concatenates two user strings

---
+++ CT2.cpp
@@ -0,0 +1,31 @@
+/*
+*CT2.cpp
+* Created by:Braden Durian
+*Jun 23, 2024
+*2:21:44 PM
+*/
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+//Function Prototype
+
+int main()
+{
+	for(int i = 0; i < 3; i++)
+	{
+		std::string string1; //use std::string to reduce vulnerabilities associated with C-strings
+		cout << "Enter your first string" << endl;
+		getline(cin, string1); //get user input for first string
+		std::string string2;
+		cout << "Enter your second string" << endl;
+		getline(cin, string2); //get user input for second string
+		std::string fullString = string1 + " " + string2; //concatenate strings
+		cout << "Your strings, concatenated, are: " + fullString << endl;
+		cout << endl; //print a space between iterations
+	}
+
+	return 0;
+}
+
